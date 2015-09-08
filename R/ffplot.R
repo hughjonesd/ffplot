66b846a14c06d956403ba078904ff80f137b776f
David Hugh-Jones <davidhughjones@gmail.com>
2015-09-08 18:01:21 +0100

    Initial README and much more

--- R/ffplot.R
+++ R/ffplot.R
@@ -8,17 +8,17 @@ fave <- function(x, ...) UseMethod("fave")
 # TODO: why does by use factor levels which aren't in data, in rhs?
 fave.formula <- function(formula, data = parent.frame(), subset = NULL) {
   subset2 <-if (! is.null(subset)) eval(substitute(subset), data) else TRUE
-  vars <- get_all_vars(formula, data) # this is the data we need. 
+  vars <- get_all_vars(formula, data) # this is the data we need.
   vars <- vars[subset2,]
   rhs <- attr(terms(formula[-2L], keep.order = TRUE), "term.labels")
   lhs <- attr(terms(formula[-3L], keep.order = TRUE), "term.labels")
   if (length(rhs) > 1) stop("Right hand side of formula has more than 1 term: ", paste(rhs, collapse = ", "))
   if (length(lhs) > 1) stop("Left hand side of formula has more than 1 term: ", paste(lhs, collapse = ", "))
-  
+
   rhs <- eval(parse(text = rhs), vars)
   if (is.factor(rhs)) rhs <- droplevels(rhs)
   result <- by(vars, rhs, function (x) eval(parse(text = lhs), x), simplify = FALSE)
-  structure(result, class = "list", colvalues = sort(unique(rhs))) 
+  structure(result, class = "list", colvalues = sort(unique(rhs)))
 }
 
 fave.default <- function(data, ...) fave.formula(..., data = data)
@@ -42,7 +42,7 @@ ffplot <- function(formula, data = parent.frame(), geom = NULL,  ..., subset = N
   lhs_all <- attr(terms(formula[-3L], keep.order = TRUE), "term.labels")
   geom_names <- c("point", "line", "errorbar", "boxplot", "barplot", "linerange", "violin", "histogram")
   if (! missing(geom)) geom <- rep_len(geom, length(lhs_all))
-  
+
   ggp <- ggplot(data = NULL)
   i <- 0
   for (lhs in lhs_all) {
@@ -55,9 +55,9 @@ ffplot <- function(formula, data = parent.frame(), geom = NULL,  ..., subset = N
     # y ~ x : multiple numbers
     # suppose if y is a factor or a character, I automatically choose a barplot of counts
     # then I could just do y ~ x
-    
-    
-    
+
+
+
     result <- fave(fml, data, subset)
     result_data <- unlist(result)
     lhsfun <- parse(text = lhs)[[1]]
@@ -65,23 +65,24 @@ ffplot <- function(formula, data = parent.frame(), geom = NULL,  ..., subset = N
     lhsfun <- as.character(lhsfun)
     lenresult <- sapply(result, length)
     # don't use "line" if rhs is a factor (or fix ggplot complaint) TODO
-    geom_name <- if (! is.null(geom)) geom[i] else 
-      if (! is.null(geom_map[[lhsfun]])) geom_map[[lhsfun]] else 
+    geom_name <- if (! is.null(geom)) geom[i] else
+      if (! is.null(geom_map[[lhsfun]])) geom_map[[lhsfun]] else
       if (is.character(result_data) || is.factor(result_data)) "histogram" else
-      if (all(lenresult == 1)) "line" else 
+      if (all(lenresult == 1)) "line" else
       if (all(lenresult == 2)) "linerange" else "point"
-  
+
+    # TODO: allow default attributes to be overridden by ..., e.g. position = "fill" below
     cv <- attr(result, "colvalues") # can be numeric, factor
     dfr <- data.frame(y = result_data, x = rep(cv, lenresult), count = unlist(lapply(lenresult, seq_len)))
-    lyr <- switch(match.arg(geom_name, geom_names), 
+    lyr <- switch(match.arg(geom_name, geom_names),
       "point"     = geom_point(aes(x = x, y = y), data = dfr, size = 3, ...),
       "line"      = geom_line(aes(x = x, y = y, group = 1), data = dfr, ...),
       "boxplot"   = geom_boxplot(aes(x = factor(x), y = y), data = dfr, ...),
-      "barplot"   = geom_bar(aes(x = x, y = y), stat = "identity", data = dfr, ...), 
-      "histogram" = geom_histogram(aes(x = x, group = y, fill = y), data = dfr, ...),
+      "barplot"   = geom_bar(aes(x = x, y = y), stat = "identity", data = dfr, ...),
+      "histogram" = geom_histogram(aes(x = x, group = y, fill = y), data = dfr, position = "fill"),
       "violin"    = geom_violin(aes(x = factor(x), y = y), data = dfr, ...),
-      "errorbar"  = stat_summary(fun.y = mean, fun.ymin = min, fun.ymax = max, mapping = aes(x = x, y = y, group = x), 
-        geom = "errorbar", width = 0.25, data = dfr, ...), 
+      "errorbar"  = stat_summary(fun.y = mean, fun.ymin = min, fun.ymax = max, mapping = aes(x = x, y = y, group = x),
+        geom = "errorbar", width = 0.25, data = dfr, ...),
       "linerange" = stat_summary(fun.y = mean, fun.ymin = min, fun.ymax = max, mapping = aes(x = x, y = y, group = x),
         geom = "linerange", data = dfr, ...)
     )
@@ -117,9 +118,9 @@ ffplot(mean(price) ~ color, d2, geom = "point", shape = 3)
 ffplot(price + quantile(price, c(0.25, 0.75)) ~ color, d2)
 ffplot(price + mean(price) ~ color, d2)
 # TODO make bars not black!
-ffplot(price + mean(price) ~ color, d2, geom = c("point", "bar")) 
-ffplot(price + mean(price) ~ color, diamonds, geom = c("violin", "line")) 
-ffplot(ci(price) ~ color, diamonds) # errorbar default 
+ffplot(price + mean(price) ~ color, d2, geom = c("point", "bar"))
+ffplot(price + mean(price) ~ color, diamonds, geom = c("violin", "line"))
+ffplot(ci(price) ~ color, diamonds) # errorbar default
 ffplot(mean(price) + ci(price) ~ color, diamonds)
 
 # without data argument
--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# ffplot
+ffplot
+======
+
+`ffplot` is the fast, friendly plotting command for R.
+
+    library(ffplot)
+    library(ggplot2)
+    data(diamonds)
+    d30 <- diamonds[1:30,]
+    head(d30)
+
+    ##   carat       cut color clarity depth table price    x    y    z
+    ## 1  0.23     Ideal     E     SI2  61.5    55   326 3.95 3.98 2.43
+    ## 2  0.21   Premium     E     SI1  59.8    61   326 3.89 3.84 2.31
+    ## 3  0.23      Good     E     VS1  56.9    65   327 4.05 4.07 2.31
+    ## 4  0.29   Premium     I     VS2  62.4    58   334 4.20 4.23 2.63
+    ## 5  0.31      Good     J     SI2  63.3    58   335 4.34 4.35 2.75
+    ## 6  0.24 Very Good     J    VVS2  62.8    57   336 3.94 3.96 2.48
+
+Simple scatterplots:
+
+    ffplot(price ~ carat, d30)
+
+![](README_files/figure-markdown_strict/unnamed-chunk-2-1.png)
+
+Plot numeric data by categories:
+
+    ffplot(price ~ color, d30)
+
+![](README_files/figure-markdown_strict/unnamed-chunk-3-1.png)
+
+Barplot of categories:
+
+    ffplot(cut ~ color, d30)
+
+![](README_files/figure-markdown_strict/unnamed-chunk-4-1.png)
+
+Plot a function of your data:
+
+    ffplot(range(price) ~ color, d30)
+
+![](README_files/figure-markdown_strict/unnamed-chunk-5-1.png)
+
+`ffplot` tries to guess what you want:
+
+    ffplot(ci(price, 0.95) ~ color, d30)
+
+    ## Warning: Removed 4 rows containing missing values (stat_summary).
+
+![](README_files/figure-markdown_strict/unnamed-chunk-6-1.png)
+
+Plot data with summary statistics:
+
+    ffplot(price + mean(price) ~ color, d30)
+
+![](README_files/figure-markdown_strict/unnamed-chunk-7-1.png)
+
+Change the defaults:
+
+    ffplot(price ~ color, diamonds, geom ="box")
+
+![](README_files/figure-markdown_strict/unnamed-chunk-8-1.png)
+
+    ffplot(price ~ color, diamonds, geom ="violin")
+
+![](README_files/figure-markdown_strict/unnamed-chunk-9-1.png)
+
+    ffplot(mean(price) ~ color, diamonds, geom = "point", shape = 3)
+
+![](README_files/figure-markdown_strict/unnamed-chunk-10-1.png)
+
+smoothing
+=========
+
+ffplot(mean(price) ~ carat, d2) \# do we smooth or what? } \# if (FALSE)
